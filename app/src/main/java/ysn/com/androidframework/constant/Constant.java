aed037ef32f96da61878d87ef01837c4853cf17a
杨三宁 <ysn_2020@163.com>
2019-06-27 22:07:00 +0800

    [ +] 增加网络请求url的打印

--- app/src/main/java/ysn/com/androidframework/constant/Constant.java
+++ app/src/main/java/ysn/com/androidframework/constant/Constant.java
@@ -1,32 +1,20 @@
 package ysn.com.androidframework.constant;
 
-public class Constant {
 
+/**
+ * @Author yangsanning
+ * @ClassName Constant
+ * @Description 常量
+ * @Date 2018/6/10
+ * @History 2018/6/10 author: description:
+ */
+public class Constant {
 
     /**
      * LOG_GLOBAL_TAG : 全局 tag
      * LOG_HTTP_REQUEST_TAG : 网络请求的 tag
      */
     public static final String LOG_GLOBAL_TAG = "test";
-    public static final String LOG_HTTP_REQUEST_TAG = "test";
-
-    /**
-     * 占位初始值
-     */
-    public static final String EMPTY_STRING = "";
-    public static final String EMPTY_JSON = "{}";
-    public static final String EMPTY_JSON_ARRAY = "[]";
-    public static final int INVALID_INT = 0;
-
-    /**
-     * page相关
-     */
-    public static final int PAGE_SIZE_DEFAULT = 20;
-    public static final int CURRENT_PAGE_DEFAULT = 1;
-
-    /**
-     * RecyclerView SPAN_COUNT
-     */
-    public static final int SPAN_COUNT_THREE = 3;
-    public static final int SPAN_COUNT_FOUR = 4;
+    public static final String LOG_HTTP_REQUEST_TAG = "httpRequest";
+    public static final String LOG_HTTP_RESPONSE_BODY_TAG = "httpResponseBody";
 }
--- app/src/main/java/ysn/com/androidframework/network/NetworkClient.java
+++ app/src/main/java/ysn/com/androidframework/network/NetworkClient.java
@@ -6,19 +6,26 @@ import com.google.gson.GsonBuilder;
 import com.lazy.library.logging.Logcat;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
 import okhttp3.CacheControl;
+import okhttp3.Headers;
 import okhttp3.Interceptor;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+import okio.Buffer;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import ysn.com.androidframework.BuildConfig;
 import ysn.com.androidframework.constant.Constant;
 import ysn.com.androidframework.constant.UrlConstant;
 import ysn.com.androidframework.network.typeadapter.DoubleTypeAdapter;
@@ -27,7 +34,11 @@ import ysn.com.androidframework.network.typeadapter.IntegerTypeAdapter;
 import ysn.com.androidframework.network.typeadapter.LongTypeAdapter;
 
 /**
- * 网络组建初始化
+ * @Author yangsanning
+ * @ClassName NetworkClient
+ * @Description 网络组建初始化
+ * @Date 2018/6/10
+ * @History 2018/6/10 author: description:
  */
 public class NetworkClient {
 
@@ -39,7 +50,7 @@ public class NetworkClient {
     /**
      * 查询缓存的Cache-Control设置，为if-only-cache时只查询缓存而不会请求服务器，max-stale可以配合设置缓存失效时间
      */
-    public static final String CACHE_CONTROL_CACHE = "only-if-cached, max-stale=" + CACHE_STALE_SEC;
+    private static final String CACHE_CONTROL_CACHE = "only-if-cached, max-stale=" + CACHE_STALE_SEC;
 
     private static NetworkClient instance = null;
     public final NetworkApiService mService;
@@ -126,12 +137,52 @@ public class NetworkClient {
     private static final Interceptor loggingInterceptor = chain -> {
         Request request = chain.request();
         Response response = chain.proceed(request);
-        MediaType mediaType = response.body().contentType();
-        String string = response.body().string();
-        Logcat.d(Constant.LOG_HTTP_REQUEST_TAG, request.url());
-        Logcat.json(Constant.LOG_HTTP_REQUEST_TAG, string);
-        return response.newBuilder()
-                .body(ResponseBody.create(mediaType, string))
-                .build();
+        if (BuildConfig.DEBUG && !"get".equalsIgnoreCase(request.method())) {
+            printRequestLog(request);
+
+            assert response.body() != null;
+            MediaType mediaType = response.body().contentType();
+            String string = response.body().string();
+            Logcat.d().tag(Constant.LOG_HTTP_RESPONSE_BODY_TAG).msg(string).out();
+            Logcat.json().tag(Constant.LOG_HTTP_RESPONSE_BODY_TAG).msg(string).out();
+            return response.newBuilder().body(ResponseBody.create(mediaType, string)).build();
+        } else {
+            return response;
+        }
     };
+
+    private static void printRequestLog(Request request) throws IOException {
+        RequestBody requestBody = request.body();
+        StringBuilder body = new StringBuilder();
+        if (requestBody != null) {
+            if (requestBody instanceof MultipartBody) {
+                MultipartBody multipartBody = (MultipartBody) requestBody;
+                for (MultipartBody.Part part : multipartBody.parts()) {
+                    Headers headers = part.headers();
+                    assert headers != null;
+                    int size = headers.size();
+                    for (int i = 0; i < size; i++) {
+                        body.append(headers.name(i));
+                        body.append(headers.value(i));
+                        if (i != size - 1) {
+                            body.append("\r\n");
+                        }
+                    }
+                }
+            } else {
+                Buffer buffer = new Buffer();
+                requestBody.writeTo(buffer);
+
+                Charset charset = Charset.forName("UTF-8");
+                MediaType contentType = requestBody.contentType();
+                if (contentType != null) {
+                    charset = contentType.charset(Charset.forName("UTF-8"));
+                }
+                assert charset != null;
+                body = new StringBuilder(buffer.readString(charset));
+            }
+        }
+        Logcat.d().tag(Constant.LOG_HTTP_REQUEST_TAG).msg(request.url()).out();
+        Logcat.d().tag(Constant.LOG_HTTP_REQUEST_TAG).msg(body.toString()).out();
+    }
 }
